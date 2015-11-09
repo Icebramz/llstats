a715601d565263955d53bc23b33b0d55fed39d6b
Joey <iphone3@ymail.com>
2015-11-08 17:04:04 -0800

    Team names now allow spaces

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ActiveRecord::Base
  
  has_many :players, dependent: :destroy
 
- validates :name, presence: true, format: {with: /\A[a-zA-Z]+\z/, message: "Only Letters are allowed"}
+ validates :name, presence: true, format: {with: /\A[a-zA-Z\s]+\z/, message: "Only Letters are allowed"}
  validates :division, presence: true, numericality: {greater_than_or_equal_to: 0}
 
 
