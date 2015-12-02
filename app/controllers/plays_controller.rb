67cd09fdf75429aec075d206d403118daa8011e4
Icebramz <icebramz@gmail.com>
2015-12-02 15:19:59 -0800

    controller for plays added. Going to be nested inside games. Also updated routes

---
+++ app/controllers/plays_controller.rb
@@ -0,0 +1,46 @@
+class TeamsController < ApplicationController
+ before_action :set_play, only: [:edit, :update, :destroy]
+
+ def new
+  @game = Game.find(params[:game_id]
+
+  @play = @game.plays.new
+ end
+
+ def edit
+ end
+
+ def create
+  @game = Game.find(params[:game_id]
+  @play = @game.plays.new(play_params)
+
+  if @play.save
+    redirect_to game_url(@game), notice: "Play was successfully created"
+  else
+   render :new
+  end 
+ end
+
+ def update
+  if @play.update(play_params)
+   redirect_to game_url(@play.game), notice: "Play was successfully updated"
+  else
+   render :edit
+  end
+ end
+
+ def destroy
+  @play.destroy
+  redirect_to game_url(@play.game), notice: "Play was successfully destroyed"
+ end
+
+ private
+  def set_play
+   @play = Play.find(params[:id])
+  end
+
+ def play_params
+  params.require(:play).permit(:inning, :player_name, :home_away, :message, :balls, :strikes, :out)
+ end
+
+end
