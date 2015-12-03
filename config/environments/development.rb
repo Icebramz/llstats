6d29ef59d8e3ba10aff8d39fb3c7d77b15916e4a
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2015-12-03 09:26:01 -0800

    Account Activation emails now work on development & production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,11 +16,23 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
+  # Don't care if the mailer can't send. <--maybe now we care
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
+
+
+  config.action_mailer.smtp_settings = {
+      :address               => "smtp.gmail.com",
+      :port                  => 587,
+      :domain                => "gmail.com",
+      :user_name             => "xyz@gmail.com",
+      :password              => "yourpassword",
+      :authentication        => :login,
+      :enable_starttls_auto  => true }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:         'smtp.gmail.com',
-    port:            587,
-    domain:          'example.com',
-    user_name:       '<username>',
-    password:        '<password>',
-    authentication:  'plain',
-    enable_starttls_auto: true }
+    :address               => "smtp.gmail.com",
+    :port                  => 587,
+    :domain                => "gmail.com",
+    :user_name             => "xyz@gmail.com",
+    :password              => "yourpassword",
+    :authentication        => :login,
+    :enable_starttls_auto  => true }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
