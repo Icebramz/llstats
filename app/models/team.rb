9c8c0c87427c5bc027dc59e5f16ad46a1d85d1c9
Icebramz <icebramz@gmail.com>
2015-12-04 18:14:15 -0800

    Changed regex for input validation in games model, allows hyphens and commas in name

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < ActiveRecord::Base
 
  has_many :players, dependent: :destroy
 
- validates :name, presence: true, format: {with: /\A[a-zA-Z\s]+\z/, message: "Only Letters are allowed"}
+ validates :name, presence: true, format: {with: /\A[a-zA-Z][a-zA-Z\-']+\z/, message: "Only Letters are allowed"}
  validates :division, presence: true, numericality: {greater_than_or_equal_to: 0}
 
  has_attached_file :avatar,
